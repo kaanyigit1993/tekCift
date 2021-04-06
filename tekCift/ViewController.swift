a64d7832eabb33e4a16a8367b174e6a8c2967834
kaanyigit1993 <>
2021-04-07 00:33:52 +0300

    en son eklennen

---
+++ tekCift/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  tekCift
+//
+//  Created by KAAN YİĞİT on 7.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var txtNumber: UITextField!
+    @IBOutlet weak var lblResult: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("asd")
+        print("asd")
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnHesapla(_ sender: Any) {
+        
+        var girilenSayi = txtNumber.text ?? "0"
+        
+        if girilenSayi.isEmpty {
+            lblResult.text = "Boş bıraktınız tekrar deneyin!"
+            return
+        }
+        
+        if let n1 = Int(girilenSayi){
+            if n1 >= 0{
+                lblResult.text = "Girilen Sayı pozitif"
+            }
+            else {
+                lblResult.text = "Girilen Sayı Negatif"
+            }
+        }else{
+            lblResult.text = "Lütfen 1 rakam giriniz!"
+        }
+        
+        
+        
+    }
+    
+}
+
---
+++ tekCift/ViewController2.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController2.swift
+//  tekCift
+//
+//  Created by KAAN YİĞİT on 8.03.2021.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    @IBAction func txtNumara(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func btnHesapla(_ sender: Any) {
+    }
+    
+}
